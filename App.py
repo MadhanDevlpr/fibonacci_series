4b555c85eccf5e628b76b204bff4bcb615f97fda
Madhan Aadithya <81558579+MadhanDevlpr@users.noreply.github.com>
2024-10-03 08:10:48 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,2 +1,10 @@
-print fibinacci
+#print fibinacci
 
+def fibonacci_for_loop(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+
+# Example usage:
+fibonacci_for_loop(10)
